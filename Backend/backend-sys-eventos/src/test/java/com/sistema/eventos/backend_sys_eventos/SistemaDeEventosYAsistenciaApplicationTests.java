6efea6efd594c560fe3aab3f00c7132af75e5f69
ArelyX1 <andrycaceres2006@gmail.com>
2025-08-06 15:13:41 -0500

    Init conf

---
+++ Backend/backend-sys-eventos/src/test/java/com/sistema/eventos/backend_sys_eventos/SistemaDeEventosYAsistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.eventos.backend_sys_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeEventosYAsistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
